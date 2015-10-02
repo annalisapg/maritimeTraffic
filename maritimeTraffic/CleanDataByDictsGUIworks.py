a14632755a2cc056c5fd362c3bd00598c78cec87
annalisapg <annalisapg@users.noreply.github.com>
2015-10-02 17:54:35 +0200

    Update CleanDataByDictsGUIworks.py

--- maritimeTraffic/CleanDataByDictsGUIworks.py
+++ maritimeTraffic/CleanDataByDictsGUIworks.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2015, Annalisa Minelli
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details <http://www.gnu.org/licenses/>.
+#
 
 import sys,subprocess,re,xlwt
 from collections import OrderedDict
